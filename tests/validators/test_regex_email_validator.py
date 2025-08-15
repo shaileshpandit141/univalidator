fc36126bc5c261fa3edf1a916265bddce3094c0b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 18:04:32 +0530

    test: add test case for valid email

--- tests/validators/test_regex_email_validator.py
+++ tests/validators/test_regex_email_validator.py
@@ -8,3 +8,22 @@ def test_regex_email_validator_isinstance(
 ) -> None:
     """Test regex validator isinstance or not."""
     assert isinstance(email, RegexEmailValidator)
+
+
+test_data: list[tuple[str, bool]] = [
+    ("user@example.com", True),
+    ("user.name@example.co", True),
+    ("user-name@sub.domain.net", True),
+    ("u@d.io", True),
+    ("test_user@my-domain.org", True),
+]
+
+
+@mark.parametrize("valid_email, expected", test_data)
+def test_valid_email_address(
+    email: RegexEmailValidator[str],
+    valid_email: str,
+    expected: bool,
+) -> None:
+    """Test valid email address."""
+    assert email.validate(valid_email) == expected
