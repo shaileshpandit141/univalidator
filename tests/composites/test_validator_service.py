553740e7cb23075c4da65e82fb8ea8be0010fe57
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 20:42:57 +0530

    test: add test case for composite email address.

--- tests/composites/test_validator_service.py
+++ tests/composites/test_validator_service.py
@@ -1,3 +1,5 @@
+from pytest import mark
+
 from univalidator.composites import CompositeValidator
 
 
@@ -6,3 +8,26 @@ def test_composite_validator_isinstance(
 ) -> None:
     """Test composite validator isinstance or not."""
     assert isinstance(composite_email, CompositeValidator)
+
+
+test_data: list[tuple[str, bool]] = [
+    ("y646896@gmail.com", True),
+    ("ios646896@gmail.com", True),
+    ("shaileshpandit141@gmail.com", True),
+    ("shaileshpandit141@dock.com", True),
+    ("user@example.com", True),
+    ("user@", False),
+    ("user@domain", False),
+    ("user@domain.", False),
+    ("user@domain.c", False),
+]
+
+
+@mark.parametrize("value, expected", test_data)
+def test_valid_email_address(
+    composite_email: CompositeValidator[str],
+    value: str,
+    expected: bool,
+) -> None:
+    """Test valid email address."""
+    assert composite_email.validate(value) == expected
