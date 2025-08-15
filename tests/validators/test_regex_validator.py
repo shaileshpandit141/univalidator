d5274300d07879584b7bc404625a85de39fe4a50
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 17:42:31 +0530

    refactor: rename fixture to usename

--- tests/validators/test_regex_validator.py
+++ tests/validators/test_regex_validator.py
@@ -1,28 +1,26 @@
-from pytest import raises
-
 from univalidator.validators import RegexValidator
 
 
 def test_regex_validator_isinstance(
-    username_validator: RegexValidator[str],
+    username: RegexValidator[str],
 ) -> None:
     """Test regex validator isinstance or not."""
-    assert isinstance(username_validator, RegexValidator)
+    assert isinstance(username, RegexValidator)
 
 
 def test_valid_username(
-    username_validator: RegexValidator[str],
+    username: RegexValidator[str],
 ) -> None:
     """Test valid username"""
-    assert username_validator.validate("username1")
-    assert username_validator.validate("username1.2")
-    assert username_validator.validate("user_name2")
+    assert username.validate("username1")
+    assert username.validate("username1.2")
+    assert username.validate("user_name2")
 
 
 def test_invalid_username(
-    username_validator: RegexValidator[str],
+    username: RegexValidator[str],
 ) -> None:
     """Test invalid username"""
-    assert not username_validator.validate("25user")
-    assert not username_validator.validate(".user")
-    assert not username_validator.validate("@user")
+    assert not username.validate("25user")
+    assert not username.validate(".user")
+    assert not username.validate("@user")
