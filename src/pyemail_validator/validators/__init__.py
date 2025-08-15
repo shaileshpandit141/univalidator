eb46b6020f67ea9ced3e6a4a4dbdcc234c967047
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 16:08:46 +0530

    feat: add mx email record validator

--- src/pyemail_validator/validators/__init__.py
+++ src/pyemail_validator/validators/__init__.py
@@ -1,4 +1,9 @@
+from ._mx_email_record_validator import MXEmailRecordValidator
 from ._regex_email_validator import RegexEmailValidator
 from ._regex_validator import RegexValidator
 
-__all__ = ["RegexValidator", "RegexEmailValidator"]
+__all__ = [
+    "MXEmailRecordValidator",
+    "RegexEmailValidator",
+    "RegexValidator",
+]
