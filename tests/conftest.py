3099097fc3a4a378b6558dc404a4e39c82ed54e2
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 17:49:44 +0530

    test: add email fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 from pytest import fixture
 
-from univalidator.validators import RegexValidator
+from univalidator.validators import RegexEmailValidator, RegexValidator
 
 
 @fixture
@@ -9,3 +9,9 @@ def username() -> RegexValidator[str]:
     return RegexValidator(
         pattern=r"^[A-Za-z][A-Za-z0-9._]{2,19}$",
     )
+
+
+@fixture
+def email() -> RegexEmailValidator[str]:
+    """Create regex email validator instance and return it."""
+    return RegexEmailValidator[str]()
