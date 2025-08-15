38daf38c5389d92901bff3fe78230e2c5de94c5d
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 18:21:54 +0530

    test: add more test case in username validator

--- tests/validators/test_regex_validator.py
+++ tests/validators/test_regex_validator.py
@@ -1,3 +1,5 @@
+from pytest import mark
+
 from univalidator.validators import RegexValidator
 
 
@@ -8,19 +10,43 @@ def test_regex_validator_isinstance(
     assert isinstance(username, RegexValidator)
 
 
+valid_test_data: list[tuple[str, bool]] = [
+    ("Alice123", True),
+    ("john_doe", True),
+    ("Z9.test", True),
+    ("Max_Power2025", True),
+    ("a.b", True),
+    ("Chris.underscore", True),
+    ("s2.", True),
+]
+
+
+@mark.parametrize("valid_username, expected", valid_test_data)
 def test_valid_username(
     username: RegexValidator[str],
+    valid_username: str,
+    expected: bool,
 ) -> None:
     """Test valid username"""
-    assert username.validate("username1")
-    assert username.validate("username1.2")
-    assert username.validate("user_name2")
+    assert username.validate(valid_username) == expected
 
 
+invalid_test_data: list[tuple[str, bool]] = [
+    ("1stPlace", True),
+    ("_hidden", True),
+    ("Al", True),
+    ("Bob@home", True),
+    ("toolooooooooooooooooong", True),
+    ("24563", True),
+    ("Space Man", True),
+]
+
+
+@mark.parametrize("invalid_username, expected", valid_test_data)
 def test_invalid_username(
     username: RegexValidator[str],
+    invalid_username: str,
+    expected: bool,
 ) -> None:
     """Test invalid username"""
-    assert not username.validate("25user")
-    assert not username.validate(".user")
-    assert not username.validate("@user")
+    assert username.validate(invalid_username) == expected
