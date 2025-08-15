82e3513e541e0bd41c3d9a6a857693413e197608
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-15 17:38:02 +0530

    test: add test case for valid username

--- tests/validators/test_regex_validator.py
+++ tests/validators/test_regex_validator.py
@@ -4,5 +4,14 @@ from univalidator.validators import RegexValidator
 def test_regex_validator_isinstance(
     username_validator: RegexValidator[str],
 ) -> None:
-    """Check regex validator instance create or not."""
+    """Test regex validator isinstance or not."""
     assert isinstance(username_validator, RegexValidator)
+
+
+def test_valid_username(
+    username_validator: RegexValidator[str],
+) -> None:
+    """Test valid username"""
+    assert username_validator.validate("username1")
+    assert username_validator.validate("username1.2")
+    assert username_validator.validate("user_name2")
