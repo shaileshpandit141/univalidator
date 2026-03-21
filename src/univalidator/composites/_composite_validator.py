487afefa1c955c07759f8426909948edb0a87bac
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-03-21 13:40:59 +0530

    feat: update composite validator base on new interface

--- src/univalidator/composites/_composite_validator.py
+++ src/univalidator/composites/_composite_validator.py
@@ -1,24 +1,23 @@
-from univalidator.abstractions import BaseCompositeValidator, BaseValidator
+from univalidator.interfaces import Validator
 
 
-class CompositeValidator[T](BaseCompositeValidator[T]):
+class CompositeValidator[T]:
     """Runs multiple validators on given data."""
 
-    def add_validator(self, validator: BaseValidator[T]) -> None:
+    def __init__(self) -> None:
+        self.validators: list[Validator[T]]
+
+    def add_validator(self, validator: Validator[T]) -> None:
         """Add a new validator."""
         self.validators.append(validator)
 
     def validate(self, value: T) -> bool:
         """Return True if all validators pass."""
-        is_valid: list[bool] = []
-        errors: list[str] = []
 
         for validator in self.validators:
             if validator.validate(value):
-                is_valid.append(True)
+                continue
             else:
-                is_valid.append(False)
-                errors.append(validator.error)
+                return False
 
-        self.errors = errors
-        return all(is_valid)
+        return True
